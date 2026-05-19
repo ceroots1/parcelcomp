e0ef67647360d675d0edd93eab5efbaa581d385f
ceroots1 <chad@consultcre.com>
2026-05-19 16:28:24 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AuthGate from './AuthGate'
+import FilterGate from './FilterGate'
 import App from './ParcelComp'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
@@ -8,7 +9,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <AuthGate>
     {({ session, onLogout }) => (
-      <App session={session} onLogout={onLogout} />
+      <FilterGate>
+        {({ countyPrefix, countyName, selectedYears, onChangeCounty }) => (
+          <App session={session} onLogout={onLogout}
+            countyPrefix={countyPrefix} countyName={countyName}
+            selectedYears={selectedYears} onChangeCounty={onChangeCounty} />
+        )}
+      </FilterGate>
     )}
   </AuthGate>
 )
