c9499dacc868eff09c829b7d19e72e28bb301208
ceroots1 <chad@consultcre.com>
2026-05-22 19:20:03 -0400

    Update index.js
    add new landing page

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LandingPage from './LandingPage'
 import AuthGate from './AuthGate'
 import FilterGate from './FilterGate'
 import App from './ParcelComp'
 
+function AppRoute() {
+  return (
+    <AuthGate>
+      {({ session, onLogout }) => (
+        <FilterGate>
+          {({ countyPrefix, countyName, selectedYears, onChangeCounty }) => (
+            <App session={session} onLogout={onLogout}
+              countyPrefix={countyPrefix} countyName={countyName}
+              selectedYears={selectedYears} onChangeCounty={onChangeCounty} />
+          )}
+        </FilterGate>
+      )}
+    </AuthGate>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-  <AuthGate>
-    {({ session, onLogout }) => (
-      <FilterGate>
-        {({ countyPrefix, countyName, selectedYears, onChangeCounty }) => (
-          <App session={session} onLogout={onLogout}
-            countyPrefix={countyPrefix} countyName={countyName}
-            selectedYears={selectedYears} onChangeCounty={onChangeCounty} />
-        )}
-      </FilterGate>
-    )}
-  </AuthGate>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/app" element={<AppRoute />} />
+    </Routes>
+  </BrowserRouter>
 )
